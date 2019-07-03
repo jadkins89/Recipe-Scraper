6fbbe3743729b2fd669f2416f1df3e920be2d7fe
Justin Adkins <jadkins1019@gmail.com>
2019-07-03 17:35:02 -0600

    updated allrecipes

--- scrapers/allrecipes.js
+++ scrapers/allrecipes.js
@@ -1,41 +1,43 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const Recipe = require('./recipe-schema');
+const request = require("request");
+const cheerio = require("cheerio");
+const Recipe = require("./recipe-schema");
 
-const allRecipes = (url) => {
-  return new Promise ((resolve, reject) => {
+const allRecipes = url => {
+  return new Promise((resolve, reject) => {
     request(url, (error, response, html) => {
       if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
-        
-        Recipe.title = $('#recipe-main-content').text();
-        
-        $('#polaris-app label').each((i, el) => {
-          const item = $(el).text().replace(/\s\s+/g, '');
-          if (item != 'Add all ingredients to list' && item != '') {
+
+        Recipe.title = $("#recipe-main-content").text();
+
+        $("#polaris-app label").each((i, el) => {
+          const item = $(el)
+            .text()
+            .replace(/\s\s+/g, "");
+          if (item != "Add all ingredients to list" && item != "") {
             Recipe.ingredients.push(item);
           }
         });
-        
-        $('.step').each((i, el) => {
-          const step = $(el).text().replace(/\s\s+/g, '');
-          if (step != '') {
+
+        $(".step").each((i, el) => {
+          const step = $(el)
+            .text()
+            .replace(/\s\s+/g, "");
+          if (step != "") {
             Recipe.instructions.push(step);
           }
         });
 
-        Recipe.time.prep = $('time[itemprop=prepTime]').text();
-        Recipe.time.cook = $('time[itemprop=cookTime]').text();
-        Recipe.time.ready = $('time[itemprop=totalTime]').text();
-        
+        Recipe.time.prep = $("time[itemprop=prepTime]").text();
+        Recipe.time.cook = $("time[itemprop=cookTime]").text();
+        Recipe.time.ready = $("time[itemprop=totalTime]").text();
+
         resolve(Recipe);
-      }
-      else {
+      } else {
         reject(error);
       }
     });
   });
-}
+};
 
 module.exports = allRecipes;
-
--- scrapers/index.js
+++ scrapers/index.js
@@ -1,14 +1,10 @@
-const allRecipes = require('./allrecipes');
-const foodNetwork = require('./foodnetwork');
+const allRecipes = require("./allrecipes");
+const foodNetwork = require("./foodnetwork");
 
-allRecipes('https://www.allrecipes.com/recipe/220727/medium-cheddar-cheese-sauce-live-raw-and-vegan/').then((recipe) => {
-  recipe.ingredients.forEach( item => {
-    console.log(item);
-  });
-  recipe.instructions.forEach( step => {
-    console.log(step + '\n');
-  });
-  console.log(recipe.time);
+allRecipes(
+  "https://www.allrecipes.com/recipe/220727/medium-cheddar-cheese-sauce-live-raw-and-vegan/"
+).then(recipe => {
+  console.log(recipe);
 });
 
 // foodNetwork('https://www.foodnetwork.com/recipes/patrick-and-gina-neely/old-fashioned-macaroni-salad-recipe-1948708').then((recipe) => {
