6640382683cff0c9313df5ecc39c3f4e2cb09071
Justin Adkins <jadkins1019@gmail.com>
2019-08-18 12:54:21 -0600

    implementing testing

---
+++ test/test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var expect = require("chai").expect;
+const allRecipes = require("../scrapers/allrecipes");
+
+describe("allRecipes", () => {
+  it("should do nothing", () => {
+    expect(true).to.equal(true);
+  });
+});
