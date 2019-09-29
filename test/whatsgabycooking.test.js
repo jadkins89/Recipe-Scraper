4c6c15fb2f759a8c7ea3af2f6b165fd53f3b5bfe
Justin Adkins <jadkins1019@gmail.com>
2019-09-29 12:43:28 -0600

    reverting back whatsGabyCooking testing

--- test/whatsgabycooking.test.js
+++ test/whatsgabycooking.test.js
@@ -1,44 +1,11 @@
 "use strict";
-const expect = require("chai").expect;
-const assert = require("chai").assert;
-
+const commonRecipeTest = require("./helpers/commonRecipeTest");
 const whatsGabyCooking = require("../scrapers/whatsgabycooking");
 const Constants = require("./constants/whatsgabycookingConstants");
 
-describe("whatsGabyCooking", () => {
-  it("should fetch the expected recipe", async () => {
-    let actualRecipe = await whatsGabyCooking(Constants.testUrl);
-    expect(JSON.stringify(Constants.expectedRecipe)).to.equal(
-      JSON.stringify(actualRecipe)
-    );
-  });
-
-  it("should throw an error if a problem occurred during page retrieval", async () => {
-    try {
-      await whatsGabyCooking(Constants.invalidUrl);
-      assert.fail("was not supposed to succeed");
-    } catch (error) {
-      expect(error.message).to.equal(
-        "No recipe found on page"
-      );
-    }
-  });
-
-  it("should throw an error if the whatsgabycooking.com doesn't contain required sub-whatsgabycooking.com", async () => {
-    try {
-      await whatsGabyCooking(Constants.invalidDomainUrl);
-      assert.fail("was not supposed to succeed");
-    } catch (error) {
-      expect(error.message).to.equal(`url provided must include 'whatsgabycooking.com/'`);
-    }
-  });
-
-  it("should throw an error if non-recipe page is used", async () => {
-    try {
-      await whatsGabyCooking(Constants.nonRecipeUrl);
-      assert.fail("was not supposed to succeed");
-    } catch (error) {
-      expect(error.message).to.equal("No recipe found on page");
-    }
-  });
-});
+commonRecipeTest(
+  "whatsGabyCooking",
+  whatsGabyCooking,
+  Constants,
+  "whatsgabycooking.com/"
+);
