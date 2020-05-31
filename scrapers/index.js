9d2811cab1d13956c9ce5b451de62125307b6e24
Justin Adkins <jadkins1019@gmail.com>
2020-05-31 12:49:22 -0600

    semantic change

--- scrapers/index.js
+++ scrapers/index.js
@@ -31,7 +31,7 @@ const domains = {
   therealfoodrds: require("./therealfoodrds"),
   thespruceeats: require("./thespruceeats"),
   whatsgabycooking: require("./whatsgabycooking"),
-  woolworths: require("./woolworths.js"),
+  woolworths: require("./woolworths"),
   yummly: require("./yummly")
 };
 
