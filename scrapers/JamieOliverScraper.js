4d481220804ba50f3316d2963e7eb51191e61969
jadkins89 <jadkins1019@gmail.com>
2021-05-20 22:17:21 -0600

    moving JamieOliver to puppeteer scrape due to latency issues, fixed up lowcarblife

--- scrapers/JamieOliverScraper.js
+++ scrapers/JamieOliverScraper.js
@@ -1,58 +1,63 @@
 "use strict";
 
-const BaseScraper = require("../helpers/BaseScraper");
+const PuppeteerScraper = require("../helpers/PuppeteerScraper");
 
-class JamieOliverScraper extends BaseScraper {
+class JamieOliverScraper extends PuppeteerScraper {
   constructor(url) {
     super(url, "jamieoliver.com/");
   }
 
   scrape($) {
     this.defaultSetImage($);
-      const { ingredients, instructions, time, tags } = this.recipe;
-      this.recipe.name = $('.single-recipe-details h1').text();
-      this.recipe.description = $("meta[property='og:description']").attr("content");
+    const { ingredients, instructions, time, tags } = this.recipe;
+    this.recipe.name = $(".single-recipe-details h1").text();
+    this.recipe.description = $("meta[property='og:description']").attr(
+      "content"
+    );
 
-      $(".tags-list a").each((i, el) => {
-          const tag = $(el)
-              .text()
-              .replace(/\s\s+/g, " ")
-              .trim();
-          if (tag !== "") {
-              tags.push(tag);
-          }
-      });
+    $(".tags-list a").each((i, el) => {
+      const tag = $(el)
+        .text()
+        .replace(/\s\s+/g, " ")
+        .trim();
+      if (tag !== "") {
+        tags.push(tag);
+      }
+    });
 
-      $("ul.ingred-list li").each((i, el) => {
-          const item = $(el)
-              .text()
-              .replace(/\s\s+/g, " ")
-              .trim();
-          if (item !== "") {
-              ingredients.push(item);
-          }
-      });
+    $("ul.ingred-list li").each((i, el) => {
+      const item = $(el)
+        .text()
+        .replace(/\s\s+/g, " ")
+        .trim();
+      if (item !== "") {
+        ingredients.push(item);
+      }
+    });
 
-      $("ol.recipeSteps li").each((i, el) => {
-          const step = $(el)
-              .text()
-              .replace(/\s\s+/g, " ")
-              .trim();
-          if (step !== "") {
-              instructions.push(step);
-          }
-      });
+    $("ol.recipeSteps li").each((i, el) => {
+      const step = $(el)
+        .text()
+        .replace(/\s\s+/g, " ")
+        .trim();
+      if (step !== "") {
+        instructions.push(step);
+      }
+    });
 
-      time.cook = $(".recipe-detail.time").text()
-          .replace('Cooks In', '')
-          .replace(/\s\s+/g, " ").trim();
+    time.cook = $(".recipe-detail.time")
+      .text()
+      .replace("Cooks In", "")
+      .replace(/\s\s+/g, " ")
+      .trim();
 
-      time.total = time.cook;
+    time.total = time.cook;
 
-      this.recipe.servings = $(".recipe-detail.serves")
-          .text()
-          .replace('Serves', '')
-          .replace(/\s\s+/g, " ").trim();
+    this.recipe.servings = $(".recipe-detail.serves")
+      .text()
+      .replace("Serves", "")
+      .replace(/\s\s+/g, " ")
+      .trim();
   }
 }
 
--- scrapers/ThatLowCarbLifeScraper.js
+++ scrapers/ThatLowCarbLifeScraper.js
@@ -13,28 +13,36 @@ class ThatLowCarbLifeScraper extends BaseScraper {
 
   scrape($) {
     this.defaultSetImage($);
+    this.defaultSetDescription($);
     const { ingredients, instructions, time } = this.recipe;
     this.recipe.name = $(".mv-create-title-primary").text();
 
-
     $(".mv-create-ingredients")
       .children("ul")
-      .children('li')
+      .children("li")
       .each((i, el) => {
         // They added a '\n' character for some reason, gotta remove it!
-        ingredients.push($(el).text().replace('\n', ''));
+        ingredients.push(this.textTrim($(el)));
       });
 
-    var inst = $('.mv-create-instructions');
-    inst.children('ol').children('li').each((i, el) => {
-      instructions.push($(el).text());
-    });
+    var inst = $(".mv-create-instructions");
+    inst
+      .children("ol")
+      .children("li")
+      .each((i, el) => {
+        instructions.push(this.textTrim($(el)));
+      });
 
     time.prep = this.textTrim($(".mv-create-time-prep .mv-create-time-format"));
-    time.cook = this.textTrim($(".mv-create-time-active .mv-create-time-format"));
-    time.total = this.textTrim($(".mv-create-time-total .mv-create-time-format"));
-    this.recipe.servings = this.textTrim($(".mv-create-time-yield .mv-create-time-format"));
-
+    time.cook = this.textTrim(
+      $(".mv-create-time-active .mv-create-time-format")
+    );
+    time.total = this.textTrim(
+      $(".mv-create-time-total .mv-create-time-format")
+    );
+    this.recipe.servings = this.textTrim(
+      $(".mv-create-time-yield .mv-create-time-format")
+    );
   }
 }
 
