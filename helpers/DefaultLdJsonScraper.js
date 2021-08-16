89e26fb384b6b359e1f2978f53133bda695e9fb3
Shani Almog <s.almog88@gmail.com>
2021-08-16 21:14:06 +0300

    don't use puppeteer in json scraper

--- helpers/DefaultLdJsonScraper.js
+++ helpers/DefaultLdJsonScraper.js
@@ -1,19 +1,19 @@
-const PuppeteerScraper = require("./PuppeteerScraper");
+const BaseScraper = require("./BaseScraper");
 
-class DefaultLdJsonScraper extends PuppeteerScraper {
+class DefaultLdJsonScraper extends BaseScraper {
 
-  async customPoll(page) {
-    let container,
-      count = 0;
-    do {
-      container = await page.$("script[type='application/ld+json']");
-      if (!container) {
-        await page.waitForTimeout(100);
-        count++;
-      }
-    } while (!container && count < 60);
-    return true;
-  }
+  // async customPoll(page) {
+  //   let container,
+  //     count = 0;
+  //   do {
+  //     container = await page.$("script[type='application/ld+json']");
+  //     if (!container) {
+  //       await page.waitForTimeout(100);
+  //       count++;
+  //     }
+  //   } while (!container && count < 60);
+  //   return true;
+  // }
 
   scrape($) {
     const isSchemaFound = this.defaultLD_JOSN($);
